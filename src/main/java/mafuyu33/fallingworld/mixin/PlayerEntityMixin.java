3d1b5e166008fb53dce8cc4214a7778ee26d34ea
mafuyu33 <jzh654354772@gmail.com>
2024-07-16 00:12:40 +0800

    基本能玩

--- src/main/java/mafuyu33/fallingworld/mixin/PlayerEntityMixin.java
+++ src/main/java/mafuyu33/fallingworld/mixin/PlayerEntityMixin.java
@@ -1,20 +1,25 @@
 package mafuyu33.fallingworld.mixin;
 
+import mafuyu33.fallingworld.FallingWorld;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.FallingBlock;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.FallingBlockEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.WaterFluid;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtHelper;
 import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
@@ -24,8 +29,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import javax.sql.rowset.Predicate;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Set;
+
+import static mafuyu33.fallingworld.FallingWorld.*;
 
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin extends LivingEntity {
@@ -35,31 +45,44 @@ public abstract class PlayerEntityMixin extends LivingEntity {
 
 	@Unique
 	private static final Set<Block> EXCLUDED_BLOCKS = new HashSet<>();
+	@Unique
+	private final Queue<BlockPos> taskQueue = new LinkedList<>();
 
 	static {
 		EXCLUDED_BLOCKS.add(Blocks.OBSIDIAN);
 		EXCLUDED_BLOCKS.add(Blocks.AIR);
+		EXCLUDED_BLOCKS.add(Blocks.LAVA);
+		EXCLUDED_BLOCKS.add(Blocks.WATER);
+		EXCLUDED_BLOCKS.add(Blocks.BEDROCK);
+		EXCLUDED_BLOCKS.add(Blocks.TALL_SEAGRASS);
+		EXCLUDED_BLOCKS.add(Blocks.TALL_GRASS);
+		EXCLUDED_BLOCKS.add(Blocks.LARGE_FERN);
+//		EXCLUDED_BLOCKS.add(Blocks.SCAFFOLDING);
 
 		// 可以在这里添加更多的需要排除的方块
 	}
-	@Unique
-	@Final
-	int fallingRadius = 7;
 
 	@Inject(at = @At("TAIL"), method = "tick")
 	private void init(CallbackInfo info) {
 		if (!this.getWorld().isClient) {
-			for (int yOffset = -fallingRadius; yOffset <= fallingRadius; yOffset++) {
-				for (int xOffset = -fallingRadius; xOffset <= fallingRadius; xOffset++) {
-					for (int zOffset = -fallingRadius; zOffset <= fallingRadius; zOffset++) {
-						BlockPos targetPos = this.getBlockPos().add(xOffset, yOffset, zOffset);
-						BlockState blockState = this.getWorld().getBlockState(targetPos);
-						Block block = blockState.getBlock();
-						if(FallingBlock.canFallThrough(this.getWorld().getBlockState(targetPos.down()))) {//如果下方方块可以掉下去
-							if (!EXCLUDED_BLOCKS.contains(block)) {
-								// 如果方块在范围内且不是特定方块，且如果方块是流体，小于等于8就忽略。
-								// 掉落！
-								fallingworld$generateFallingBlock(targetPos, blockState, this.getWorld());
+			int fallingRadius_x = this.getWorld().getGameRules().getInt(FALLING_RANGE_HORIZONTAL);
+			int fallingRadius_y = this.getWorld().getGameRules().getInt(FALLING_RANGE_VERTICAL);
+			int fallingRadius_z = this.getWorld().getGameRules().getInt(FALLING_RANGE_HORIZONTAL);
+
+			if(fallingRadius_x!=0 && fallingRadius_y!=0 && fallingRadius_z!=0) {//都不为0
+				for (int yOffset = -fallingRadius_y; yOffset <= fallingRadius_y; yOffset++) {
+					for (int xOffset = -fallingRadius_x; xOffset <= fallingRadius_x; xOffset++) {
+						for (int zOffset = -fallingRadius_z; zOffset <= fallingRadius_z; zOffset++) {
+							BlockPos targetPos = this.getBlockPos().add(xOffset, yOffset, zOffset);
+							BlockState blockState = this.getWorld().getBlockState(targetPos);
+							Block block = blockState.getBlock();
+							if (FallingBlock.canFallThrough(this.getWorld().getBlockState(targetPos.down()))) {//如果下方方块可以掉下去
+								if (!EXCLUDED_BLOCKS.contains(block)) {
+									// 如果方块在范围内且不是特定方块。掉落！
+									fallingworld$generateFallingBlock(targetPos, blockState, this.getWorld());
+								} else if (fallingworld$isFluidExceedingThreshold(targetPos)) {//如果方块是流体源头，处理他的掉落
+									fallingworld$generateFallingBlock(targetPos, blockState, this.getWorld());
+								}
 							}
 						}
 					}
@@ -69,12 +92,20 @@ public abstract class PlayerEntityMixin extends LivingEntity {
 	}
 
 	@Unique
-	private boolean fallingworld$isFluidExceedingThreshold(BlockState blockState) {
+	private boolean fallingworld$isFluidExceedingThreshold(BlockPos blockPos) {
+		BlockState blockState = this.getWorld().getBlockState(blockPos);
 		if (blockState.getBlock() == Blocks.WATER || blockState.getBlock() == Blocks.LAVA) {
-			int level = blockState.get(Properties.LEVEL_1_8);
-			return level < 8 && level >= 0; // 忽略流动中的水和岩浆,还未实现
+			int level = blockState.get(Properties.LEVEL_15);
+			if (level==0){//如果是源头的话
+				//底下是不是可以掉落的部分?如果是液体，就掉落
+                return fallingworld$canFluidFallThrough(this.getWorld().getBlockState(blockPos.down()));
+			}
 		}
 		return false;
+	}
+	@Unique
+	private static boolean fallingworld$canFluidFallThrough(BlockState state) {
+		return state.isAir() || state.isIn(BlockTags.FIRE);
 	}
 	@Unique
 	private void fallingworld$generateFallingBlock(BlockPos targetPos ,BlockState blockState, World world) {
@@ -93,8 +124,6 @@ public abstract class PlayerEntityMixin extends LivingEntity {
 		fallingBlockEntity.prevY = targetPos.getY();
 		fallingBlockEntity.prevZ = targetPos.getZ() + 0.5;
 		fallingBlockEntity.setFallingBlockPos(targetPos);
-
-		world.spawnEntity(fallingBlockEntity);
 		// 如果方块有附加的 BlockEntity 数据，可以设置 blockEntityData 字段
 		BlockEntity blockEntity = world.getBlockEntity(targetPos);
 		if (blockEntity != null) {
@@ -102,6 +131,9 @@ public abstract class PlayerEntityMixin extends LivingEntity {
 			blockEntity.writeNbt(blockEntityData);
 			fallingBlockEntity.blockEntityData = blockEntityData;
 		}
+
+		world.spawnEntity(fallingBlockEntity);
+
 	}
 }
 
